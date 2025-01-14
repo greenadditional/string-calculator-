7d547f1b9d03a83776eb2b8009f57a9550277d98
thesaurizateone <166837657+thesaurizateone@users.noreply.github.com>
2025-01-14 07:25:52 -0800

    Add files via upload

---
+++ stringcalculator.lua
@@ -0,0 +1,6 @@
+string='it is an lua calculator made with string'
+string='it can be done online and with the community'
+string='if there are any question about string, you can search'
+string='any object is mutable by default and lived through translation'
+string='must known lua5.1 and can  communicate for worldview'
+
